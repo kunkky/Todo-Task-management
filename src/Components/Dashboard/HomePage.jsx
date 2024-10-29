29edc75c84c7d547b85ccdd830cadbdc9675faca
kunkky <83945048+kunkky@users.noreply.github.com>
2024-10-29 05:18:02 +0100

    Update HomePage.jsx

--- src/Components/Dashboard/HomePage.jsx
+++ src/Components/Dashboard/HomePage.jsx
@@ -7,13 +7,17 @@ const HomePage = () => {
       <nav className=" w-full top-0  flex items-center justify-between p-6 bg-gray-900 text-white">
         <div className="text-xl font-bold">TaskMaster</div>
         <div className="space-x-4 text-xs">
-          <a href="/signin" className="hover:text-gray-300">
+          <a href="/login" className="hover:text-gray-300">
             Sign in
           </a>
           <a href="/register" className="hover:text-gray-300">
             Register
           </a>
-          <a href="#contact" className="hover:text-gray-300">
+          <a
+            href="https://documenter.getpostman.com/view/25136449/2sAY4uD43J"
+            target="_blank"
+            className="hover:text-gray-300"
+          >
             Developer's Doc
           </a>
         </div>
